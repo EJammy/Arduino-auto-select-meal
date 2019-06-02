81043feb25841bde523995b157d94137df0302f9
Connorc627 <Connorc0627@gmail.com>
2019-06-02 19:00:01 +0800

    more comments

--- OneButtonBaffet/OneButtonBaffet.ino
+++ OneButtonBaffet/OneButtonBaffet.ino
@@ -1,5 +1,4 @@
 #include <Keyboard.h>
-//note: would not work when there is more than 4 full
 const int loadTime = 3000;//change this according to the web page load time
 bool button;
 void setup() {
@@ -11,38 +10,49 @@ void setup() {
 }
 
 void pressButton(int n, int wait){
+  //presses n times of tab and press that button
   for(int i = 0; i < n; i++){
     Keyboard.write(179);
   }
   delay(50);
   Keyboard.write('\n');
-  if(wait){
+  if(wait){ //delay if wait is 1
     delay(loadTime);
   }
 }
 
 void loop() {
-  button = digitalRead(8);
-  Serial.println(button);
-  if(button){
+  button = digitalRead(8);//set the button state
+  Serial.println(button);//for debug
+  
+  if(button){//when button is pressed
     for(int i = 0; i < 5; i++){
+      
       pressButton(3 + i, 1);
       //Due to the way the website is set up
       //" + i" is only required when meal is already chosen
-      pressButton(17, 0);
+      //When meal is not chosen yet
+      //delete it and use "pressButton(3, 1);" instead
+      
+      pressButton(17, 0);//choses the next 17 button, the buffet
+      
       //In case there is more than 2 meal that are full,
-      //doesn't work very well
+      //the button would skip and not chose meal
+      //This part of the code
+      //goes back to chose if the page hasn't loaded yet
+      //__might not work__
       delay(50);
       Keyboard.press(129);
       Keyboard.write(179);
       Keyboard.write(179);
       Keyboard.releaseAll();
       Keyboard.write('\n');
+      //
 
       delay(loadTime);
-      pressButton(13, 0);
+      pressButton(13, 0);//press comfirm
       delay(500);
-      Keyboard.write(179);
+      Keyboard.write(179);//one extra tab is needed
       delay(500);
     }
     delay(1000);
